5cff9a8e9e181ee9c297db599418e6cb1833d8f7
wuyan <958144600@qq.com>
2021-06-11 16:13:56 +0800

    Revert "修改项目文件路径"
    This reverts commit 3234e95

--- src/main/java/com/doctor/dao/SysMenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.doctor.dao;
-
-import com.doctor.model.SysPermission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SysMenuRepository extends JpaRepository<SysPermission,Long> {
-}
--- src/main/java/com/doctor/dao/SysRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.doctor.dao;
-
-import com.doctor.model.SysRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SysRoleRepository extends JpaRepository<SysRole,Long> {
-
-
-}
